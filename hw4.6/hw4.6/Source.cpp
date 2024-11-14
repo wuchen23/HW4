4f0bde01c17018cb8ef2a32e82d2da6994bd9298
wuchen23 <brianwugame950619@gmail.com>
2024-11-14 22:55:00 +0800

    Add files via upload

---
+++ hw4.6/hw4.6/Source.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+void printarray(const int a[][3]);
+int main(void) {
+	int array1[2][3] = { {1,2,3},{4,5,6} };
+	int array2[2][3] = { 1,2,3,4,5 };
+	int array3[2][3] = { {1,2},{4} };
+	printf("Values in arrayl by row are: \n");
+	printarray(array1);
+	printf("Values in array2 by row are: \n");
+	printarray(array2);
+	printf("Values in array3 by row are: \n");
+	printarray(array3);
+	system("pause");
+	return 0;
+}
+
+void printarray(const int a[][3]) {
+	int i;
+	int j;
+	for (i = 0; i <= 1; i++)
+	{
+		for (j = 0; j <= 2; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
